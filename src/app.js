9873b193c85fb51fe3b20d04bb1a3f800378a704
James <jtronlabs@gmail.com>
2018-11-14 11:03:55 -0800

    test cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 //import packages
 var express = require("express");
 var bodyParser = require("body-parser");
+var cors = require("cors");
 
 //import route functions
 const StripeRoute = require("./StripeRoute");
@@ -16,20 +17,31 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const allowedOrigins = ["http://localhost:3000", "https://wfpb.fit"];
-app.use(function(req, res, next) {
-  //read request and if it's from the allowedOrigins add that origin to header for CORS
-  let origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin); // restrict it to the required domain
-  }
+app.use(
+  cors({
+    origin: [".*localhost.*"]
+  })
+); 
+app.options("*", cors()); // include before other routes
 
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
+// const allowedOrigins = [
+//   // "http://localhost:3000",
+//   "https://wfpb.fit"
+// ];
+// app.use(function(req, res, next) {
+//   //read request and if it's from the allowedOrigins add that origin to header for CORS
+//   let origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.header("Access-Control-Allow-Origin", origin); // restrict it to the required domain
+//   }
+
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 //Connect functions to API routes
 app.post("/charge", StripeRoute);
